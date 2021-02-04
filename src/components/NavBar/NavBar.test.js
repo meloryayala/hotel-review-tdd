e0bfa75982e826f60a3a691e189cda4fa2f09ab3
Melory Ayala <melory.ayala@gmail.com>
2021-02-04 17:01:26 +0100

    Testing NavBar with screen

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -2,6 +2,9 @@ import {render, screen} from "@testing-library/react";
 import NavBar from "./NavBar";
 
 test('The NavBar component should render', () => {
-    const view = render(<NavBar title='Test application' />);
-    expect(view).toMatchSnapshot();
+
+    const title = 'Test application';
+    render(<NavBar title={title} />);
+
+    expect(screen.getByRole('heading')).toHaveAttribute(title)
 })
